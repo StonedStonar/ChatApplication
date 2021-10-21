1720c3fd37afc354022a0ed347ab2c710b341c40
StonedStonar <steinarhjellem@gmail.com>
2021-10-21 11:32:50 +0200

    Added the methods to testingclasses for personal conversation and register.

--- src/test/no/stonedstonar/chatappliation/model/TestPersonalConversation.java
+++ src/test/no/stonedstonar/chatappliation/model/TestPersonalConversation.java
@@ -1,4 +1,8 @@
 package no.stonedstonar.chatappliation.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 /**
  * A class that tests methods in the PersonalConversation class.
  * @version 0.1
@@ -10,74 +14,163 @@ public class TestPersonalConversation {
     /**
      * Tests if the constructor works with invalid ServerConversation.
      */
+    @Test
+    @DisplayName("Tests if the constructor works with invalid ServerConversation.")
+    public void testIfConstructorWorksWithInvalidServerConversation(){
+
+    }
 
     /**
      * Tests if constructor works with invalid username.
      */
+    @Test
+    @DisplayName("Tests if constructor works with invalid username.")
+    public void testIfConstructorWorksWithInvalidUsername(){
+
+    }
 
     /**
      * Tests if constructor works with valid input.
      */
+    @Test
+    @DisplayName("Tests if constructor works with valid input.")
+    public void testIfConstructorWorksWithValidInput(){
+
+    }
 
     /**
      * Tests if setConversationName works with invalid input.
      */
+    @Test
+    @DisplayName("Tests if setConversationName works with invalid input.")
+    public void testIfSetConversationNameWorksWithInvalidInput(){
+
+    }
 
     /**
      * Tests if setConversationName works with valid input.
      */
+    @Test
+    @DisplayName("Tests if setConversationName works with valid input.")
+    public void testIfSetConversationNameWorksWithValidInput(){
+
+    }
 
     /**
      * Tests if checkForMessageLogByDate works with valid input.
      */
+    @Test
+    @DisplayName("Tests if checkForMessageLogByDate works with valid input.")
+    public void testIfCheckForNewMessageLogByDateWorksWithValidInput(){
+
+    }
 
     /**
      * Tests if addNewMessage works with invalid input.
      */
+    @Test
+    @DisplayName("Tests if addNewMessage works with invalid input")
+    public void testIfAddNewMessageWorksWithInvalidInput(){
+
+    }
 
     /**
      * Tests if addNewMessage works with valid message.
      */
+    @Test
+    @DisplayName("Tests if addNewMessage works with valid message.")
+    public void testIfAddNewMessageWorksWithValidMessage(){
+
+    }
 
     /**
      * Tests if addNewMessage works with duplicate messages.
      */
+    @Test
+    @DisplayName("Tests if addNewMessage works with duplicate messages.")
+    public void testIfAddNewMessageWorksWithDuplicateMessages(){
+
+    }
 
     /**
      * Tests if removeMessage works with invalid input.
      */
+    @Test
+    @DisplayName("Tests if removeMessage works with invalid input.")
+    public void testIfRemoveMessageWorksWithInvalidInput(){
+
+    }
 
     /**
      * Tests if removeMessage works with valid input.
      */
+    @Test
+    @DisplayName("Tests if removeMessage works with valid input.")
+    public void testIfRemoveMessageWorksWithValidInput(){
+
+    }
 
     /**
      * Tests if removeMessage works with message not in conversation.
      */
+    @Test
+    @DisplayName("Tests if removeMessage works with message not in conversation.")
+    public void testIfRemoveMessageWorksWithMessageNotInConversation(){
+
+    }
 
     /**
      * Tests if addMessagesWithSameDate works with invalid list.
      */
+    @Test
+    @DisplayName("Tests if addMessagesWithSameDate works with invalid list.")
+    public void testIfAddMessageWithSameDateWorksWtihInvalidList(){
+
+    }
 
     /**
      * Tests if addMessagesWithSameDate works with valid input.
      */
+    @Test
+    @DisplayName("Tests if addMessagesWithSameDate works with valid input.")
+    public void testIfAddMessagesWithSameDateWorksWithValidInput(){
+
+    }
 
     /**
      * Tests if addMessagesWithSameDate works with one invalid date.
      */
+    @Test
+    @DisplayName("Tests if addMessagesWithSameDate works with one invalid date.")
+    public void testIfAddMessagesWithSameDateWorksWithInvalidDate(){
+
+    }
 
     /**
      * Tests if getMessageLogForDate works with invalid input.
      */
+    @Test
+    @DisplayName("Tests if getMessageLogForDate works with invalid input.")
+    public void testIfGetMessageLogForDateWorksWithInvalidInput(){
+
+    }
 
     /**
      * Tests if getMessageLogForDate works with valid input.
      */
+    @Test
+    @DisplayName("Tests if getMessageLogForDate works with valid input.")
+    public void testIfGetMessageLogForDateWorksWithValidInput(){
+
+    }
 
     /**
      * Tests if getMessageLogForDate works with invalid date.
      */
+    @Test
+    @DisplayName("Tests if getMessageLogForDate works with invalid date.")
+    public void testIfGetMessageLogForDateWorksWithInvalidDate(){
 
+    }
 
 }
--- src/test/no/stonedstonar/chatappliation/model/TestPersonalConversationRegister.java
+++ src/test/no/stonedstonar/chatappliation/model/TestPersonalConversationRegister.java
@@ -1,4 +1,8 @@
 package no.stonedstonar.chatappliation.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 /**
  *
  * @version 0.1
@@ -9,37 +13,82 @@ public class TestPersonalConversationRegister {
     /**
      * Tests if constructor works with invalid normalMessageLogList.
      */
+    @Test
+    @DisplayName("Tests if constructor works with invalid normalMessageLogList.")
+    public void testIfConstructorWorksWithInvalidNormalMessageLogList(){
+
+    }
 
     /**
      * Tests if constructor works with invalid username.
      */
+    @Test
+    @DisplayName("Tests if constructor works with invalid username.")
+    public void testIfConstructorWorksWithInvalidUsername(){
+
+    }
 
     /**
      * Tests if constructor works with valid input.
      */
+    @Test
+    @DisplayName("Tests if constructor works with valid input.")
+    public void testIfConstructorWorksWithValidInput(){
+
+    }
 
     /**
      * Tests if addConversation works with invalid conversation.
      */
+    @Test
+    @DisplayName("Tests if addConversation works with invalid conversation.")
+    public void testIfAddConversationWorksWithInvalidConversation(){
+
+    }
 
     /**
      * Tests if addConversation works with invalid username.
      */
+    @Test
+    @DisplayName("Tests if addConversation works with invalid username.")
+   public void testIfAddConversationWorksWithInvalidUsername(){
+
+    }
 
     /**
      * Tests if addConversation works with valid username.
      */
+    @Test
+    @DisplayName("Tests if addConversation works with valid username.")
+    public void testIfAddConversationWorksWithValidUsername(){
+
+    }
 
     /**
      * Tests if getConversationByNumber works with invalid conversation number.
      */
+    @Test
+    @DisplayName("Tests if getConversationByNumber works with invalid conversation number.")
+    public void testIfGetConversationByNumberWorksWithInvalidConversationNumber(){
+
+    }
 
     /**
      * Tests if getConversationByNumber works with valid input.
      */
+    @Test
+    @DisplayName("Tests if getConversationByNumber works with valid input.")
+    public void testIfGetConversationByNumberWorksWithValidInput(){
+
+    }
 
     /**
      * Tests if getConversationByNumber works with conversation number not in register.
      */
+    @Test
+    @DisplayName("Tests if getConversationByNumber works with conversation number not in register.")
+    public void testIfGetConversationByNumberWorksWithConversationNotInRegister(){
+        
+    }
 
 }
